78c49eddc704577d617e60352cdf64b7c4cb970b
Robin Jonsson <robin.jonsson@desenio.com>
2021-11-19 10:35:53 +0100

    i18n + middleware

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['en', 'be', 'be-de', 'be-nl', 'be-fr'],
+    defaultLocale: 'en',
+    domains: [
+      {
+        domain: 'example.be',
+        defaultLocale: 'be',
+        locales: ['be-de', 'be-nl', 'be-fr'],
+      },
+    ],
+  },
+};
